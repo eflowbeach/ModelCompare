e3d2e9fbed26cbfa6970b3ee3b5f16b79ef5f635
jonathan.wolfe <jonathan.wolfe@noaa.gov>
2014-12-19 21:15:29 -0500

    Adding some checks and auto initialize data fields

--- source/class/mc/Application.js
+++ source/class/mc/Application.js
@@ -77,42 +77,77 @@ qx.Class.define("mc.Application",
       -------------------------------------------------------------------------
       */
       var me = this;
-      var sites = ["KCRW", "Snowshoe"];
+      var sites = new qx.data.Array();
+      me.fields = new qx.data.Array();
+      me.models = new qx.data.Array();
+        me.ready = new qx.data.Array();
+ d3.text("http://dev.nids.noaa.gov/~jwolfe/ModelCompare/data/models.csv", function(text)
+                            {
+                           // text = text.substring(0, text.length - 1);
+                            var sortedModels = d3.csv.parseRows(text)[0].sort();
+                   me.models.append(sortedModels);
+                   me.ready.append([true]);
+                  })
+
+       d3.text("http://dev.nids.noaa.gov/~jwolfe/ModelCompare/data/sites.csv", function(text)
+                {
+                //text = text.substring(0, text.length - 1);
+                var sortedSites = d3.csv.parseRows(text)[0].sort();
+       sites.append(sortedSites);
+       me.ready.append([true]);
+      })
+
+      d3.text("http://dev.nids.noaa.gov/~jwolfe/ModelCompare/data/fields.csv", function(text)
+                      {
+                      //text = text.substring(0, text.length - 1);
+                      var sortedFields = d3.csv.parseRows(text)[0].sort();
+             me.fields.append(sortedFields);
+             me.ready.append([true]);
+            })
+
+
+
       var win = new qx.ui.window.Window("Controls");
       win.setMinWidth(200);
       win.setLayout(new qx.ui.layout.VBox());
 
-      //         setTimeout(function(){
       me.getRoot().add(win,
       {
         left : 900,
         top : 100
       });
 
-      //                }, 2000);
+
       win.open();
       me.site = new mc.JQx.SelectBox();
-      var optionsUpper = new qx.data.Array(sites);
-      var optionsUpperController = new qx.data.controller.List(optionsUpper, me.site);
+
+      var optionsUpperController = new qx.data.controller.List(sites, me.site);
       win.add(me.site);
       me.site.addListener("changeSelection", function(e) {
         me.test();
       }, this);
       me.field = new mc.JQx.SelectBox();
-      var optionsUpper = ["SnowAmt", "PoP"];
-      optionsUpper = optionsUpper.sort();
-      var optionsUpperController = new qx.data.controller.List(new qx.data.Array(optionsUpper), me.field);
+
+
+      var optionsUpperController = new qx.data.controller.List(me.fields, me.field);
       win.add(me.field);
       me.field.addListener("changeSelection", function(e) {
         me.test();
       }, this);
 
       //              me.field.setSelection([me.field.getSelectables()[0]]);
-      this.test();
+setTimeout(function(){
+      me.test();
+},300);
+
     },
     test : function()
     {
       var me = this;
+
+
+      // check to make sure all initialization files have been loaded
+      if(me.ready.length!=3){return;}
 
       // Clear
       d3.select("body").selectAll(".horizon").remove();
@@ -158,7 +193,7 @@ qx.Class.define("mc.Application",
       .step(3600 * 1000).size(240 * 5 + 2).stop();
       var horizon = context.horizon()
       d3.select("body").append("div").attr("class", "rule").call(context.rule());
-      d3.select("body").selectAll(".horizon").data(["Fcst", "CONSAll", "GFS40", "ECMWFHiRes", "AllBlend", "CMCnh", "CONSMOS", "CONSRaw", "HPCGuide", "HRRR", "MOSGuide", "NAM12", "NamDNG5", "CMCreg", "ADJMAV", "ADJMET", "RUC13", "SREF", "wrfar5", "wrfarw", "wrfnmm"].map(stock)).enter().insert("div", ".bottom").attr("class", "horizon").call(context.horizon().format(d3.format("+,.2r")));
+      d3.select("body").selectAll(".horizon").data(me.models.toArray().map(stock)).enter().insert("div", ".bottom").attr("class", "horizon").call(context.horizon().format(d3.format("+,.2r")));
 
       //      d3.select("body").selectAll(".horizon").call(horizon.colors(['rgb(241,238,246)','rgb(189,201,225)','rgb(116,169,207)','rgb(5,112,176)','rgb(241,238,246)','rgb(189,201,225)','rgb(116,169,207)','rgb(5,112,176)'])).call(horizon.scale([0,100]));
       context.on("focus", function(i)
