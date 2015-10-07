e6d07485d6c0398b44d1ab7cc3507b4e847fad83
Jonathan Wolfe <jonathan.wolfe@noaa.gov>
2015-10-07 14:08:56 -0400

    Add a user agent

--- source/resource/mc/getConfig.php
+++ source/resource/mc/getConfig.php
@@ -4,6 +4,7 @@ include_once("./util/sanitize.php");
 // jSON URL which should be requested
 $wfo = isset($_REQUEST["wfo"]) ? sanitize($_REQUEST["wfo"], "") : 0;
 
+$userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36';
 
 $json_url = "http://www.weather.gov/source/".$wfo."/modelcompare/config.json";
 
@@ -12,6 +13,7 @@ $ch = curl_init( $json_url );
 
 // Configuring curl options
 $options = array(
+        CURLOPT_USERAGENT=> $userAgent,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_HTTPHEADER => array('Content-type: application/json')
 );
--- source/resource/mc/getConfigDetails.php
+++ source/resource/mc/getConfigDetails.php
@@ -4,6 +4,8 @@ include_once("./util/sanitize.php");
 // jSON URL which should be requested
 $details = isset($_REQUEST["details"]) ? $_REQUEST["details"] : 0;
 
+$userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36';
+
 
 $json_url = $details;//"http://www.weather.gov/source/".$wfo."/modelcompare/config.json";
 
@@ -12,6 +14,7 @@ $ch = curl_init( $json_url );
 
 // Configuring curl options
 $options = array(
+        CURLOPT_USERAGENT=> $userAgent,
 		CURLOPT_RETURNTRANSFER => true//,
 		//CURLOPT_HTTPHEADER => array('Content-type: application/json')
 );
